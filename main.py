ee06f4481480f6dc6c08bde90a445e6a66754985
Felipe Acosta <fjad20@gmail.com>
2025-01-05 00:26:51 +0100

    Cambiando la carga del PDF y probando cambiando el prompt

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import time
 import re
 from dotenv import load_dotenv, find_dotenv
-from langchain_community.document_loaders import PDFMinerLoader
+from langchain_community.document_loaders import PDFMinerLoader, PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from pinecone import Pinecone
@@ -10,95 +10,127 @@ from langchain_core.documents import Document
 from langchain_community.vectorstores import Pinecone as PineconeVectorStore
 from pinecone import ServerlessSpec
 from langchain.chains.question_answering import load_qa_chain
-from langchain.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate, ChatPromptTemplate
 from langchain_core.messages import HumanMessage, SystemMessage
 
 # Cargando la variable de entorno           - OK
 load_dotenv(find_dotenv(), override=True) 
 
-# Cargando el documento                     - OK
-pdf_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'pdf_files', 'document_1.pdf') 
-loader = PDFMinerLoader(pdf_file_path)
-document = loader.load()[0]
 
-# Cargando el modelo de embeddings          - OK
-embedding = OpenAIEmbeddings()
+# Cargando el documento                     - OK
+def load_document(filename, loader = 'pdfminer'):
+    '''
+    filename: str
+    loader: 'pdfminer' or 'pypdf'
+    '''
+    pdf_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'pdf_files', filename) 
+
+    if loader == 'pdfminer':
+        loader = PDFMinerLoader(pdf_file_path, concatenate_pages= True)
+        # loader = PyPDFLoader(pdf_file_path)
+        document = loader.load()[0]
+    elif loader == 'pypdf':
+        loader = PyPDFLoader(pdf_file_path)
+        document = loader.load()
+    
+    print('Document loaded!')
+
+    return document
 
 # Funcion para dividir el texto en chunks   - OK
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size = 512,
-    chunk_overlap = 0,
-    separators=[""])
-chunks = text_splitter.split_text(document.page_content)
+def text_into_chunks(document):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 512,
+                                            chunk_overlap = 50,
+                                            separators=[""])
+    chunks = text_splitter.split_documents(documents = document)
+    print('Text splitted into chunks!')
+    
+    return chunks
+
+# Cargando el modelo de embeddings          - OK
+def get_embedding_model():
+    embedding = OpenAIEmbeddings()
+    return embedding
 
 # Creamos un cliente de Pinecone            - OK
-pc = Pinecone()
+def load_pinecone(index_name):
 
-index_name = 'tenders-index'
+    pc = Pinecone()
 
-# Creamos indice en Pinecone o lo cargamos si ya existe - OK
-if index_name not in pc.list_indexes().names():
-    
-    for i in pc.list_indexes().names():
-        pc.delete_index(i)
-        print('Index deleted')
-        print('-' * 50)
-    
-    print(f'Creating index {index_name}')
-    pc.create_index(
-        name=index_name,
-        dimension=1536,
-        metric='cosine',
-        spec=ServerlessSpec(
-            cloud="aws",
-            region="us-east-1"
-        ) 
+    # Creamos indice en Pinecone o lo cargamos si ya existe - OK
+    if index_name not in pc.list_indexes().names():
+        
+        for i in pc.list_indexes().names():
+            pc.delete_index(i)
+            print('Index deleted')
+            print('-' * 50)
+        
+        print(f'Creating index {index_name}')
+        pc.create_index(
+            name=index_name,
+            dimension=1536,
+            metric='cosine',
+            spec=ServerlessSpec(
+                cloud="aws",
+                region="us-east-1"
+            ) 
+        )
+        print('Index created!')
+        while not pc.describe_index(index_name).status['ready']:
+            time.sleep(1)
+            print('Index ready!')
+
+    else:
+        index = pc.Index(index_name)
+        print(f'Index {index_name} loaded!')
+        if index.describe_index_stats().total_vector_count > 0:
+            index.delete(delete_all=True)
+            print('Content deleted!')
+
+# Creamos y cargamos la base vectorial en Pinecone      - OK
+def load_vector_store(chunks, index_name):
+
+    vector_store = PineconeVectorStore.from_documents(
+        documents= chunks,
+        embedding=get_embedding_model(),
+        index_name=index_name
     )
-    print('Index created!')
-    while not pc.describe_index(index_name).status['ready']:
-        time.sleep(1)
-        print('Index ready!')
 
-else:
-    index = pc.Index(index_name)
-    print(f'Index {index_name} loaded!')
-    index.delete(delete_all=True)
-    print('Content deleted!')
+    vector_store = PineconeVectorStore.from_existing_index(index_name=index_name, embedding=get_embedding_model())
+    print('Vector store loaded!')
+    
+    return vector_store
 
-# Creamos la base vectorial en Pinecone      - OK
-vector_store = PineconeVectorStore.from_texts(
-    texts= chunks,
-    embedding=embedding,
-    index_name=index_name
-)
 
-# Cargamos la base vectorial en Pinecone     - OK 
-vstore = PineconeVectorStore.from_existing_index(index_name=index_name, embedding=embedding)
-
-# ------------------------------------------------------------------------------------------------------
 # PRUEBA DE FUNCIONAMIENTO
-# ------------------------------------------------------------------------------------------------------
+index_name = 'tenders-index'
+document = load_document('document_1.pdf', loader = 'pypdf')
+chunks = text_into_chunks(document)
+load_pinecone(index_name)
+vector_store = load_vector_store(chunks, index_name)
 
-query = '¿Cual es el objeto del expediente?'
+retriever = vector_store.as_retriever(search_type = 'similarity')
+time.sleep(10)
+relevant_chunks = retriever.invoke('¿Cual es el objeto del expediente?')
 
-message = [
-    # SystemMessage(content = 'You are a helpull asistant, Your name is Wall-E'),
-    HumanMessage(content = query)
-]
+# prompt template
+PROMPT_TEMPLATE = '''
+Eres un asistente para preguntar y responder. Usa los contextos para responder la pregunta. Si no sabes que responder, solo di que no sabes. No te inventes las respuestas.
+
+{context}
+-----
+Answer the question based on the above context: {question}
+'''
+content_text = "\n\n---\n\n".join([chunk.page_content for chunk in relevant_chunks])
+
+# Create prompt
+prompt_template = PromptTemplate.from_template(PROMPT_TEMPLATE)
+prompt = prompt_template.format(context=content_text, question='¿Cual es el objeto del expediente?')
 
 llm = ChatOpenAI(model='gpt-3.5-turbo')
 
-docs = vstore.similarity_search(query=query, k=3)
+response = llm.invoke(prompt)
 
-chain = load_qa_chain(
-    llm = llm,
-    chain_type = 'stuff'
-)
+print(response.content)
 
-response = chain.invoke(input_documents = docs , question = query)
-
-print(response)
-
-
-print('Done!')
